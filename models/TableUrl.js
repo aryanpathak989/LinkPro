0b5976437820868f573d6a5e61e5f6e0ad9c2915
aryanpathak989 <aryanpathak738@gmail.com>
2025-04-10 00:45:13 +0530

    saving chnages

--- models/TableUrl.js
+++ models/TableUrl.js
@@ -1,21 +1,21 @@
-const { DataTypes, Sequelize, INTEGER } = require("sequelize");
-const sequelize = require("../lib/database");
+// const { DataTypes, Sequelize, INTEGER } = require("sequelize");
+// const sequelize = require("../lib/database");
 
-const Url = sequelize.define("tblurl",{
-    id:{
-        type:INTEGER,
-        primaryKey:true,
-        autoIncrement:true,
-        unique:true,
-    },
-    urlId:{
-        type:DataTypes.STRING,
-        unique:true
-    },
-    actualUrl:{
-        type:DataTypes.STRING,
-        allowNull:false
-    }
-})
+// const Url = sequelize.define("tblurl",{
+//     id:{
+//         type:INTEGER,
+//         primaryKey:true,
+//         autoIncrement:true,
+//         unique:true,
+//     },
+//     urlId:{
+//         type:DataTypes.STRING,
+//         unique:true
+//     },
+//     actualUrl:{
+//         type:DataTypes.STRING,
+//         allowNull:false
+//     }
+// })
 
-module.exports = Url
+// module.exports = Url
